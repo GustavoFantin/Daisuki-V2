7a12134744f9b6c6a0e5368d797d46bfc6adbc38
Phil <xuyuwei19940909@gmail.com>
2025-06-12 11:41:31 -0700

    fix header bug

--- final-project/src/app/contact-us/page.tsx
+++ final-project/src/app/contact-us/page.tsx
@@ -1,11 +1,19 @@
+'use client'
+
 import { Toaster } from "react-hot-toast"
 import Background from "../about/Background"
 import ContactForm from "./ContactForm"
+import { useState } from "react";
+import Header from "@/components/Header";
+import BurgerMenu from "@/components/BurgerMenu";
 
 const page = () => {  
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
   <>
+    <Header setMenuOpen={setMenuOpen} />
+    <BurgerMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     <Toaster />
     <Background />
     <ContactForm />
