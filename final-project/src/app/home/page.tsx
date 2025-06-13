a9b8e1e564bb61e440fcaa30b37101448d57e5fc
Phil <xuyuwei19940909@gmail.com>
2025-06-13 00:49:51 -0700

    modified home page position

--- final-project/src/app/home/page.tsx
+++ final-project/src/app/home/page.tsx
@@ -1,12 +1,22 @@
 'use client'
 
-import './styles.css'
+import  './styles.css'
 import { Background } from './Background'
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { useEffect } from 'react';
+
 
 export default function NewHome() {
   const router = useRouter();
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden'
+
+    return () => {
+      document.body.style.overflow = ''
+    }
+  }, [])
 
   return <>
       <Background />
@@ -14,23 +24,45 @@ export default function NewHome() {
         className='flex lg:justify-start items-end justify-center '
         style={{ position: 'absolute', pointerEvents: 'none', top: 0, left: 0, width: '100vw', height: '100vh' }}
       >
-        <div className="char" style={{ top: 40, left: 40 }}>
-          D
+        <div className='hidden md:block'>
+          <div className="char" style={{ top: 40, left: 40 }}>
+            D
+          </div>
+          <div className="char" style={{ top: 40, left: '20vw' }}>
+            A
+          </div>
+          <div className="char" style={{ top: 40, left: '40vw' }}>
+            I
+          </div>
+          <div className="char" style={{ top: '20vw', left: 40 }}> 
+            S
+          </div>
+          <div className="char" style={{ bottom: 40, right: '30vw' }}>
+            U
+          </div>
+          <div className="char" style={{ bottom: 40, right: 40 }}>
+            KI
+          </div>
         </div>
-        <div className="char" style={{ top: 40, left: '20vw' }}>
-          A
-        </div>
-        <div className="char" style={{ top: 40, left: '40vw' }}>
-          I
-        </div>
-        <div className="char" style={{ top: '20vw', left: 40 }}>
-          S
-        </div>
-        <div className="char" style={{ bottom: 40, right: '30vw' }}>
-          U
-        </div>
-        <div className="char" style={{ bottom: 40, right: 40 }}>
-          KI
+        <div className='block md:hidden'>
+          <div className="char" style={{ top: 70, left: 40 }}>
+            D
+          </div>
+          <div className="char" style={{ top: 70, left: '20vw' }}>
+            A
+          </div>
+          <div className="char" style={{ top: 70, left: '40vw' }}>
+            I
+          </div>
+          <div className="char" style={{ top: '35vw', left: 40 }}> 
+            S
+          </div>
+          <div className="char" style={{ bottom: 100, right: '30vw' }}>
+            U
+          </div>
+          <div className="char" style={{ bottom: 100, right: 40 }}>
+            KI
+          </div>
         </div>
         <div style={{ position: 'absolute', top: 80, right: 40, fontSize: '18px', textAlign: 'right', color: 'white' }}>
           {/* Find The Perfect Partner for You
