73a169911005e46397adef370b16396f98d56f80
Phil <xuyuwei19940909@gmail.com>
2025-06-13 00:07:32 -0700

    fix burger problem

--- final-project/src/app/service-list/page.tsx
+++ final-project/src/app/service-list/page.tsx
@@ -125,7 +125,7 @@ export default function RentalGirlfriendList() {
                     {/* Girls listing */}
                     {girls.length > 0 ? (
                         <div
-                            className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:px-20 lg:px-30 px-10"
+                            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:px-20 lg:px-30 px-10"
                         >
                             {girls.map((girl, index) => (
                                 <div
